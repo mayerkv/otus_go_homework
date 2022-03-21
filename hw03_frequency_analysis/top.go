cefddd90deb154faaa819bb95b6e643c96c98f94
kmayer <k.v.mayer@tm2.etagi.com>
2022-03-21 17:14:22 +0500

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,56 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCount struct {
+	w   string
+	cnt int
+}
+
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	if len(words) == 0 {
+		return nil
+	}
+
+	wordsCountMap := map[string]int{}
+	for _, word := range words {
+		wordsCountMap[word]++
+	}
+
+	wordsCountSlice := make([]wordCount, 0, len(wordsCountMap))
+
+	for w, cnt := range wordsCountMap {
+		wordsCountSlice = append(wordsCountSlice, struct {
+			w   string
+			cnt int
+		}{w, cnt})
+	}
+
+	sort.Slice(wordsCountSlice, func(i, j int) bool {
+		a, b := wordsCountSlice[i], wordsCountSlice[j]
+
+		if a.cnt == b.cnt {
+			return strings.Compare(a.w, b.w) < 0
+		}
+
+		return a.cnt > b.cnt
+	})
+
+	wordsCount := 10
+	if len(wordsCountSlice) < wordsCount {
+		wordsCount = len(wordsCountSlice)
+	}
+
+	wordsCountSlice = wordsCountSlice[:wordsCount]
+
+	res := make([]string, 0, wordsCount)
+	for _, item := range wordsCountSlice {
+		res = append(res, item.w)
+	}
+
+	return res
 }
