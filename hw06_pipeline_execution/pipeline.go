f4962d5eba5ae8df9fca33fd151ac744264e84d4
kmayer <k.v.mayer@tm2.etagi.com>
2022-04-25 12:38:57 +0500

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,46 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	pipeIn := make(Bi)
+	pipeOut := pipeIn
+
+	for _, s := range stages {
+		pipeOut = withDone(done, pipeOut, s)
+	}
+
+	go func() {
+		defer close(pipeIn)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			case pipeIn <- i:
+			}
+		}
+	}()
+
+	return pipeOut
+}
+
+func withDone(done In, in In, stage Stage) Bi {
+	out := make(Bi)
+	results := stage(in)
+
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case r, ok := <-results:
+				if !ok {
+					return // channel is closed
+				}
+
+				out <- r
+			}
+		}
+	}()
+
+	return out
 }
