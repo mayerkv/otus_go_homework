204a594766a587beee934d871927c6d200552181
kmayer <k.v.mayer@tm2.etagi.com>
2022-05-18 16:30:12 +0500

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,60 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	src, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+
+	stat, err := src.Stat()
+	if err != nil {
+		return err
+	}
+	if !stat.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset > stat.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	maxLimit := stat.Size() - offset
+	if limit > maxLimit || limit == 0 {
+		limit = maxLimit
+	}
+
+	dst, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	if _, err := src.Seek(offset, 0); err != nil {
+		return err
+	}
+
+	var total, chunkSize int64 = 0, 1 << 20
+	if chunkSize > limit {
+		chunkSize = limit
+	}
+
+	bar := pb.New64(limit)
+	bar.Set(pb.Bytes, true)
+	bar.Start()
+	defer bar.Finish()
+
+	for total < limit {
+		written, err := io.CopyN(dst, src, chunkSize)
+		bar.Add64(written)
+		total += written
+
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,38 @@
 package main
 
-import "testing"
+import (
+	"io/fs"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("when source file not exists, then returns error", fileNotExists)
+
+	t.Run("when source is directory, then returns error", sourceIsDirectory)
+
+	t.Run("when offset grate then file size", offsetGrateThenFileSize)
+}
+
+func fileNotExists(t *testing.T) {
+	err := Copy("non_existent_file", "/dev/null", 0, 0)
+
+	require.Error(t, err)
+	require.IsType(t, &fs.PathError{}, err)
+}
+
+func sourceIsDirectory(t *testing.T) {
+	err := Copy("testdata", "/dev/null", 0, 0)
+
+	require.Error(t, err)
+	require.ErrorIs(t, ErrUnsupportedFile, err)
+}
+
+func offsetGrateThenFileSize(t *testing.T) {
+	var offset int64 = 7000
+	err := Copy("testdata/input.txt", "/dev/null", offset, 0)
+
+	require.Error(t, err)
+	require.ErrorIs(t, ErrOffsetExceedsFileSize, err)
 }
