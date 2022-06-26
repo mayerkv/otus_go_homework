168797846a7da67a0bd314f5b16363ab3ca1efc4
Konstantin Mayer <k.v.mayer@esoft.local>
2022-06-26 14:20:32 +0500

    hw11 completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -13,9 +16,48 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &simpleClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type simpleClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
+func (c *simpleClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connection error: %w", err)
+	}
+	c.conn = conn
+
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", c.address)
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *simpleClient) Close() error {
+	return c.conn.Close()
+}
+
+func (c *simpleClient) Send() error {
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return fmt.Errorf("sending error: %w", err)
+	}
+	return nil
+}
+
+func (c *simpleClient) Receive() error {
+	if _, err := io.Copy(c.out, c.conn); err != nil {
+		return fmt.Errorf("receiving error: %w", err)
+	}
+	return nil
+}
