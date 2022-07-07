93764bfd7392bea9f645d7df348170928a2ed31c
kmayer <k.v.mayer@tm2.etagi.com>
2022-07-07 20:33:56 +0500

    add configuration and command endpoint

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,20 +1,37 @@
 package main
 
-// При желании конфигурацию можно вынести в internal/config.
-// Организация конфига в main принуждает нас сужать API компонентов, использовать
-// при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
+import "github.com/spf13/viper"
+
 type Config struct {
 	Logger LoggerConf
-	// TODO
+	HTTP   HTTPConf
 }
 
 type LoggerConf struct {
 	Level string
-	// TODO
 }
 
 func NewConfig() Config {
 	return Config{}
 }
 
-// TODO
+type HTTPConf struct {
+	Port int
+}
+
+func ReadConfig(path string) (*Config, error) {
+	v := viper.New()
+	v.AutomaticEnv()
+	v.SetConfigFile(path)
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := NewConfig()
+	if err := v.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -1,3 +1,8 @@
-module github.com/fixme_my_friend/hw12_13_14_15_calendar
+module github.com/mayerkv/otus_go_homework/hw12_13_14_15_calendar
 
 go 1.16
+
+require (
+	github.com/spf13/cobra v1.5.0
+	github.com/spf13/viper v1.12.0
+)
